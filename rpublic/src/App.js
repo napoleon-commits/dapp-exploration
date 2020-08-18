7ffb265609b5f76c6d40e29e7b492c1077cb861c
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2020-08-17 20:57:02 -0500

    added web3 dependency and logged contract and accounts

--- rpublic/src/App.js
+++ rpublic/src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Web3 from 'web3';
+
 function App() {
+  var contractABI = [];
+  var contractAddress = "0xcaa0a4D9bA721f6921Fe0e6136dE72E51a7845Bb";
+  var web3 = new Web3('http://127.0.0.1:8545');
+  var simpleSmartContract = new web3.eth.Contract(contractABI, contractAddress);
+  console.log('simpleSmartContract');
+  console.log(simpleSmartContract);
+  console.log('simpleSmartContract');
+  console.log('-------------------');
+  console.log('-------------------');
+  web3.eth.getAccounts()
+  .then(console.log);
   return (
     <div className="App">
       <header className="App-header">
