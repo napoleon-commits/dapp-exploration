9c674791a1613e19c73746bf511ead882362e2f6
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2020-08-17 20:27:29 -0500

    prepped front end dev

--- test/simpleSmartContract.js
+++ test/simpleSmartContract.js
@@ -3,7 +3,6 @@ const SimpleSmartContract = artifacts.require('SimpleSmartContract');
 contract('SimpleStorageContract', () => {
     it('Should deploy smart contract properly', async () => {
         const simpleSmartContract = await SimpleSmartContract.deployed();
-        console.log(simpleSmartContract.address);
         assert(simpleSmartContract.address != '');
     });
 });
